990feeb3068afa5e6617460919bb1cfeadfdc2f1
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-06-01 15:57:04 -0400

    cleaned up JS

--- js/rotate.js
+++ js/rotate.js
@@ -15,35 +15,34 @@ $(window).load(function(){
 
 	// Center images vertically in space
 	var mainHeight = $('#main').height(),
-		gallery = $('.gallery');
-	gallery.height(0.85714 * gallery.width()); // set height to 6/7 of width (proper for images)
+		gallery = $('.gallery'),
+		galHeight;
 
+	function centerGallery() {
+		// if window larger than 600px, 
+		// set height to 6/7 of width (proper for images).
+		// otherwise 1.25 * width (since tablet/mobile get larger)
+		galHeight = $(window).width() > 600 ? 0.85714 * gallery.width() : 1.25 * gallery.width();
+		gallery.height(galHeight);
 		if (mainHeight > gallery.height()) {
 			gallery.css({
 				'top': (mainHeight - gallery.height())/2 - 45
 			});
 		}
-	
-	$(window).resize(function(){
-		mainHeight = $('#main').height(),
-		gallery.height(0.85714 * $('.gallery').width());
-		if (mainHeight > gallery.height()) {
-			gallery.css({
-				'top': (mainHeight - gallery.height())/2 - 45
-			});
-		}
-	});
+	}
+	centerGallery();
+	$(window).resize(centerGallery);
 
 	// Auto-rotate
 	var rotators = $('.rotate img'),
 		active, next, prev;
 
 	var goNext = function(){
-		active = $('.rotate .active');
+		active = $('.rotate .active, #controls .active');
 		active.stop().each(function(){
 			$this = $(this);
 			// Next is the next image, or if we're at the last one, the first
-			next = $this.next().length > 0 ? $this.next() : $this.closest('.rotate').find('img, div').first();
+			next = $this.next().length ? $this.next() : $this.parent().find('img, div').first();
 			// Make this one inactive and the next one active
 			if ($this.parent().attr('id') == 'controls') {
 				$this.removeClass('active');
@@ -60,11 +59,11 @@ $(window).load(function(){
 	}, 4000);
 
 	var goPrev = function(){
-		active = $('.rotate .active');
+		active = $('.rotate .active, #controls .active');
 		active.stop().each(function(){
 			$this = $(this);
 			// Prev is the prev image, or if we're at the first one, the last
-			prev = $this.prev().length > 0 ? $this.prev() : $this.closest('.rotate').find('img, div').last();
+			prev = $this.prev().length ? $this.prev() : $this.parent().find('img, div').last();
 			// Make this one inactive and the prev one active
 			if ($this.parent().attr('id') == 'controls') {
 				$this.removeClass('active');
@@ -86,22 +85,18 @@ $(window).load(function(){
 
 	// Controls
 	var num,
-		controls = $('#controls').css({
-			'left': 0.5 * gallery.width() - 34, // 34 is half of the controls' width
-			'top': gallery.height()
+		controls = $('#controls'),
+		visit = $('.visit.eighthundred');
+	function posControls() {
+		controls.css({
+			'left': 0.5 * gallery.width() - 34 // 34 is half of the controls' width
 		}),
 		visit = $('.visit.eighthundred').css({
 			'top': gallery.height() + 30
 		});
-	$(window).resize(function(){
-		controls.css({
-			'left': 0.5 * gallery.width() - 34,
-			'top': gallery.height()
-		});
-		visit.css({
-			'top': gallery.height() + 30
-		})
-	});
+	}
+	posControls();
+	$(window).resize(posControls);
 
 	controls.find('div').click(function(){
 		$this = $(this);
@@ -111,29 +106,24 @@ $(window).load(function(){
 
 		rotators.each(function(){
 			active = $('#desktop .active, #tablet .active, #mobile .active');
-			next = $(this).index() == num ? $(this) : undefined;
+			next = $(this).index() == num ? $(this) : false;
 
 			// If same index, fade in and hide the active one
-			if (next) {
-				next.addClass('active').fadeIn().siblings('.active').removeClass('active').fadeOut();
-			}
+			if (next) next.addClass('active').fadeIn().siblings('.active').removeClass('active').fadeOut();
 		});
 	});
 
 	// Desktop / tablet / mobile
 	var devices = $('.devices img'),
-		devicesArray = new Array('desktop', 'tablet', 'mobile');
+		devicesArray = ['desktop', 'tablet', 'mobile'];
 	devices.click(function(){
 		$this = $(this);
 		num = $this.index() - 1; // minus one for the 'choose:' paragraph
 
 		$this.addClass('active').siblings().removeClass('active');
-
-		for (i=0; i<devicesArray.length; i++) {
-			if ($this.hasClass(devicesArray[i])) {
-				$('.gallery #'+devicesArray[i]).addClass('chosen').siblings().removeClass('chosen');
-			}
-		}
+		
+		// Choose the images for this one and unchoose the others
+		$('.gallery #' + devicesArray[num]).addClass('chosen').siblings().removeClass('chosen');
 	});
 
 });
